6fc8f1f7027e7164c7bab876faab764902aebfd1
Jorge_de_la_Torre <jorgedlt@gmail.com>
2017-03-26 20:46:59 -0400

    Commit message

--- mkhex.sh
+++ mkhex.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+let p=0
+let k=0
+
 # Usage: $0 <count>
 N=$1
 while (( N-- > 0 )); do
   # use bc to convert hex to decimal
-  openssl rand -hex 32 | xargs openssl prime -hex | awk '/is prime/ {print "ibase=16;"$1}' | bc
+  result=$(openssl rand -hex 16 | xargs openssl prime -hex | awk '/is prime/ {print "ibase=16;"$1}' | bc)
+
+[ -z "$result" ] && ((k=k+1)) ||  { ((p=p+1)); echo "$result" >> primes.log ; }
+
 done
 
-srouce - http://stackoverflow.com/questions/19732162/openssl-generate-prime-number
+echo
+echo primes $p
+echo k $k
+echo
+
+# source - http://stackoverflow.com/questions/19732162/openssl-generate-prime-number
